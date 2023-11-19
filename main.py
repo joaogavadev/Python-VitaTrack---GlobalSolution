c7414cec13f4b182247dc09f75bd91801555ed46
joaogavadev <gavadevworks@gmail.com>
2023-11-19 11:54:52 -0300

    Menu melhorado e rotas de login consertados

--- main.py
+++ main.py
@@ -26,10 +26,12 @@ def cadastro():
         password2 = getpass.getpass()
     else:
         print("Cadastro finalizado.")
-        with open(r'C:\Users\gava\Documents\PROGRAMAÇÃO\ARQUIVOS PYTHON\GS\logins.csv', 'w', newline="") as f:
+        with open(r'C:\Users\gava\Documents\PROGRAMAÇÃO\ARQUIVOS PYTHON\GS\logins.csv', 'a', newline="") as f:
             validador_csv = csv.writer(f)
             validador_csv.writerow(cadastro_registro)
 
+        print("\nCadastro finalizado. Agora você pode fazer login.")
+        login()
 def login():
     print("==============================================================")
     print("Login\n\n")
@@ -37,10 +39,21 @@ def login():
     username = input("Usuário: ")
     password_login = getpass.getpass()
 
-    if validar_login(username, password_login, email_user):
-        print("Login bem-sucedido!")
-    else:
-        print("Usuário ou senha incorretos.")
+    while not validar_login(username, password_login, email_user):
+        login_incorreto = input("Usuario, email ou senha incorretos. Digite 1 para tentar novamente ou Digite 2 para retornar: ")
+        if login_incorreto == '2':
+            introdução()
+            return
+        elif login_incorreto == '1':
+            email_user = input("\nEmail: ")
+            username = input("Usuário: ")
+            password_login = getpass.getpass()
+        else:
+            print("Opção inválida. Digite 1 para tentar novamente ou Digite 2 para retornar.")
+
+    print("\nLogin bem-sucedido!")
+    input("Pressione Enter para acessar nosso menu...")
+    menu()
 
 def validar_login(username, password, email_user):
     with open(r'C:\Users\gava\Documents\PROGRAMAÇÃO\ARQUIVOS PYTHON\GS\logins.csv', 'r', newline="") as f:
@@ -54,19 +67,31 @@ def validar_login(username, password, email_user):
         
 def introdução():
     print("==============================================================")
-    print("Seja bem vindo a nossa plataforma VitaTrack!")
+    print("                   Bem-vindo à VitaTrack!")
+    print("==============================================================")
 
 
-    print("\nVocê já é um usuario ou gostaria de se cadastrar? \n-Digite '1' se já for cadastrado. \n-Digite '2' se gostaria de realizar o cadastro)")
-    user_choise = input("")
+    print("Escolha uma opção: \n1. Fazer Login.\n2. Cadastrar\n3. Sair")
+    user_choise = input()
     match user_choise:
         case '1':
             login()
         case '2':
             cadastro()
+        case '3':
+            print("Obrigado por usar a VitaTrack. Até logo!")
+        case _:
+            print("Opção Invalida, Escolha uma opção válida.")
+            introdução()
 
+
+def menu():
+    print("==============================================================")
+    print("                           MENU")
+    print("==============================================================")
 
 introdução()
+
 
 
 
