b3daf7164026c600afeb2e4a9de011df857492db
joaogavadev <gavadevworks@gmail.com>
2023-11-19 17:01:19 -0300

    Cadastro e login finalizado, adicionando função de calculo de IMC e inicio do menu com base nos dados registrados

--- main.py
+++ main.py
@@ -4,13 +4,73 @@ import csv
 #inicio do programa
 
 
+def registro_info():
+    #adicionar texto
+    print("==============================================================")
+    print("Registro")
+
+    nome = input("\nDigite seu primeiro nome: ")
+    peso = input("Digite seu peso (Em kg): ")
+    idade = input("Digite sua idade: ")
+    altura = input("Digite sua altura: ")
+    container_userinfo = [nome,idade,peso,altura]
+    criar_csv_usuario(nome, container_userinfo)
+
+#colunas criadas respectivamente: |NOME - PESO - IDADE - ALTURA|
+def criar_csv_usuario(nome, container_userinfo):
+    path = f'C:/Users/gava/Documents/PROGRAMAÇÃO/ARQUIVOS PYTHON/GS/csv/user_dados/{nome}_dados.csv'
+    with open(path, 'w', newline='') as f:
+        writer = csv.writer(f)
+        #infos puxadas + convertendo
+        container_userinfo = [str(nome), int(container_userinfo[1]), int(container_userinfo[2]), float(container_userinfo[3])]
+        writer.writerow(container_userinfo)
+    leitor_userinfo(nome)
+
+def leitor_userinfo(nome):
+    path = f'C:/Users/gava/Documents/PROGRAMAÇÃO/ARQUIVOS PYTHON/GS/csv/user_dados/{nome}_dados.csv'
+    with open(path, 'r', newline='') as f:
+        leitor = csv.reader(f)
+        for linha in leitor:
+            nome, idade, peso, altura = linha
+            print(type(str(nome)))
+            print(type(int(idade)))
+            print(type(int(peso)))
+            print(type(float(altura)))
+
+
+    #adicionar texto
+    IMC = (int(peso)/float(altura)** 2)
+    IMC_arredondado = round(IMC, 2)
+    print(f"Seu IMC é {IMC_arredondado}")
+    if IMC_arredondado >= 40:
+        print("Seu estado é de Obesidade grau III")
+
+
+    elif IMC_arredondado >= 35 and IMC_arredondado <= 39.9:
+        print("Seu estado é de Obesidade grau II")
+
+    elif IMC_arredondado >= 35 and IMC_arredondado <= 39.9:
+        print("Seu estado é de Obesidade grau II")
+
+    elif IMC_arredondado >= 30 and IMC_arredondado <= 34.9:
+        print("Seu estado é de Obesidade grau I")
+
+    elif IMC_arredondado >= 25 and IMC_arredondado <= 29.9:
+        print("Seu estado é de Sobrepeso")
+
+    elif IMC_arredondado >= 18.6 and IMC_arredondado <= 24.9:
+        print("Seu estado é Normal.")
+
+    elif IMC_arredondado < 18.5:
+        print("Seu estado é Abaixo do normal.")
+
+
+
 def cadastro():
     print("==============================================================")
     print("Cadastro")
     email_user = input("\n\nEmail: ")
-
-    username = input("nome de usuario: ")
-
+    username = input("Nome de usuário: ")
     password = getpass.getpass()
 
     print("Confirme sua senha novamente:")
@@ -18,20 +78,20 @@ def cadastro():
     cadastro_registro = [email_user, username, password]
 
     while password != password2:
-
         print("As senhas não coincidem, Digite sua senha:")
         password = getpass.getpass()
-
         print("Confirme sua senha novamente:")
         password2 = getpass.getpass()
     else:
         print("Cadastro finalizado.")
-        with open(r'C:\Users\gava\Documents\PROGRAMAÇÃO\ARQUIVOS PYTHON\GS\logins.csv', 'a', newline="") as f:
+        with open(r'C:\Users\gava\Documents\PROGRAMAÇÃO\ARQUIVOS PYTHON\GS\csv\logins.csv', 'a', newline="") as f:
             validador_csv = csv.writer(f)
             validador_csv.writerow(cadastro_registro)
 
+        # Cria o CSV do usuário com os seguintes conteúdos
+        registro_info()
+
         print("\nCadastro finalizado. Agora você pode fazer login.")
-        login()
 def login():
     print("==============================================================")
     print("Login\n\n")
@@ -85,10 +145,15 @@ def introdução():
             introdução()
 
 
+
+
+
 def menu():
     print("==============================================================")
     print("                           MENU")
     print("==============================================================")
+
+    print()
 
 introdução()
 
